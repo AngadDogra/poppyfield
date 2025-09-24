c889fb1e824f4efbfcdaa6d10e483d3f24d0a9bd
Angad Dogra <angaddogra1204@gmail.com>
2025-09-24 13:12:10 +0530

    added another change

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, world, this is a poppy field!")
+    return HttpResponse("Hello, world, this is a poppy field and this is another change!")
 
